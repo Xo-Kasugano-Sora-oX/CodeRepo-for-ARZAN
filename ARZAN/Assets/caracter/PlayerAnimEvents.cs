a84ba87850ca5c9877653094099a970bf098f939
zHOUKANG00139 <122763952+zHOUKANG00139@users.noreply.github.com>
2025-09-21 10:37:07 +0800

    update

---
+++ ARZAN/Assets/caracter/PlayerAnimEvents.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimEvents : MonoBehaviour
+{
+    private PlayerController player;
+    void Start()
+    {
+        player = GetComponentInParent<PlayerController>();
+    }
+    private void AnimationTrgger()
+    {
+        player.AttackOver();
+    }
+}
--- ARZAN/Assets/caracter/玩家移动配置.cs
+++ ARZAN/Assets/caracter/玩家移动配置.cs
@@ -7,6 +7,8 @@ using UnityEngine;
 using UnityEngine.UIElements;
 using UnityEngine.UI;
 using Image = UnityEngine.UI.Image;
+using UnityEngine.Rendering.Universal;
+using TMPro;
 public class PlayerController : MonoBehaviour
 
 {
@@ -14,29 +16,37 @@ public class PlayerController : MonoBehaviour
     public Rigidbody2D rb;
     [Header("test anim")]
     [SerializeField] private Animator PAnim;
+    [SerializeField] private bool IsMoving;
+    [SerializeField] private bool isDashing;
+    [SerializeField] private bool isJumping;
 
     [Header("Move control")]
     private bool FacingRight = true;
-    public bool SHIT1ON;
     private float xInput;
     public float MoveSpeedPlayer = 4f;
     public float acceleration = 10f;
     public float maxSpeed = 8f;
-    private float velocityY;
     //"JUMP"
     public float JumpForce = 8f;
+    public float JumpCount = 1f;
+    public float JumpCounter;
     public LayerMask groundLayer;
     public Transform groundCheck;
     public float checkRadius = 0.2f;
     [Header("Attack Settings")]
     private bool isAttacking;
-    private Animator anim;
+    private int combocounter;
 
-
-    
     void Start()
     {
         PAnim = GetComponentInChildren<Animator>();
+    }
+    void FixedUpdate()
+    {
+        if (Mathf.Abs(rb.velocity.x) < maxSpeed)
+        {
+            rb.AddForce(new Vector2(xInput * acceleration, 0), ForceMode2D.Force);
+        }
     }
     void Update()
     {
@@ -50,34 +60,8 @@ public class PlayerController : MonoBehaviour
         {
             Flip();
         }
-
-        /*从这里*/
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            JumpForce = JumpForce + 1f;
-            Debug.Log("+1");
-        }
-        if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            JumpForce = JumpForce - 1f;
-            Debug.Log("-1");
-        }
-        /*到这里，为了方便调试特地加的*/
-
-        //获得按键输入为A、D分别输出-1、1
-        xInput = Mathf.Abs(Input.GetAxis("Horizontal")) > 0.1f ? Input.GetAxis("Horizontal") : 0f;
-
+        //水平移动
         rb.velocity = new Vector2(MoveSpeedPlayer * 2 * xInput, rb.velocity.y);
-
-
-        if (Input.GetKeyDown(KeyCode.J))//跳跃按键检测
-        {
-            Debug.Log("你是不是尝试按跳跃键了");
-            rb.velocity = new Vector2(rb.velocity.x, JumpForce);
-        }
-
-        // 原有速度检测逻辑
-
 
         // 新增跳跃状态检测
         if (!isGrounded)
@@ -96,24 +80,21 @@ public class PlayerController : MonoBehaviour
                 PAnim.SetBool("IsMoving", IsMoving = false);
             }
         }
-
-
-
-
-        // 跳跃缓冲
+        // 跳跃
         if (Input.GetKeyDown(KeyCode.J) && isGrounded)
         {
             rb.velocity = new Vector2(rb.velocity.x, JumpForce * 2);
+            JumpCounter -= 1f;
         }
-        //地面检查（下一步）
+        //地面检查
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, groundLayer);//(监测点（父）， 半径 ， 图层)
 
-        //冲刺！！！！！！
-
+        AnimatoerControllers();
+        InputChecker();
     }
     private float RushTime = 2f;
     private float RushTimer;
-    
+
     private bool isGrounded;//布尔值判断真假
     //实现换方向时人物翻转
     void Flip()
@@ -124,9 +105,6 @@ public class PlayerController : MonoBehaviour
         transform.localScale = scale;
     }
     //animations
-    [SerializeField] private bool IsMoving;
-    [SerializeField] private bool isRushing;
-    [SerializeField] private bool isJumping;
 
     //SKILL 1 冲刺
     [Header("DASH INFO")]
@@ -135,17 +113,26 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float DashCoolDown = 0.5f;
     [SerializeField] private float Dashtimer = 5f;
 
-    //移动物理优化
-    void FixedUpdate()
-    {
-        if (Mathf.Abs(rb.velocity.x) < maxSpeed)
-        {
-            rb.AddForce(new Vector2(xInput * acceleration, 0), ForceMode2D.Force);
-        }
-    }
-    //跳跃缓冲
-    public float jumpBufferTime = 0.2f;
-    private float jumpBufferCounter;
 
+    private void AnimatoerControllers()
+    {
+        PAnim.SetBool("isAttacking", isAttacking);
+        PAnim.SetInteger("combocounter", combocounter);
+        PAnim.SetBool("isGrunded", isGrounded);
+    }
+    //atack
+    public void AttackOver()
+    {
+        isAttacking = false;
+    }
+    void InputChecker()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            isAttacking = true;
+            
+        }
+        xInput = Mathf.Abs(Input.GetAxis("Horizontal")) > 0.1f ? Input.GetAxis("Horizontal") : 0f;
+    }
 
 }
